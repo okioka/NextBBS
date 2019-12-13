f6c304ceae594acf0311959660ead148e6cef788
okioka <komiya417@gmail.com>
2019-12-13 21:38:24 +0900

    レンダリングで出ていた警告を出ないようにした

--- next-app/pages/index.js
+++ next-app/pages/index.js
@@ -1,13 +1,12 @@
 import css from "../public/styles.scss"
-import {data, CommentBox, CommentForm} from "../components/App.js"
+import App from "../components/App.js"
 
 const Index = () => (
   <div className={css.center}>
     <div className={css.back}>
       <h1>掲示板</h1>
       <div className={css.bbs}>
-        <div id='content'><CommentBox data={data} /></div>
-        <CommentForm /> 
+        <App />
       </div>
     </div>
   </div>

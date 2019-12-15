d41b2112a1cd786989a5fa5a3c778baa8b2e8c32
okioka <komiya417@gmail.com>
2019-12-16 00:55:30 +0900

    投稿を管理するテーブルの作成

---
+++ api/DB/201912160030_create_posts.sql
@@ -0,0 +1,8 @@
+CREATE TABLE BBS.posts (
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    user VARCHAR(20) NOT NULL,
+    comment VARCHAR(255) NOT NULL,
+    post_date TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    /* delete_flg 0:デフォルト値 1:削除 */
+    delete_flg VARCHAR(1) NOT NULL 
+);

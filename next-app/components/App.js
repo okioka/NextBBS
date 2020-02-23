4fdb1d0b7a4d0ab3d23d05b4830ac79a09a351dc
okioka <komiya417@gmail.com>
2020-02-23 16:31:45 +0900

    document.getElementByIdの使用を避ける

--- next-app/components/App.js
+++ next-app/components/App.js
@@ -19,7 +19,7 @@ export default class CommentBox extends React.Component {
       const posts = res.data["posts"]
       this.setState({posts: posts})
     }).catch(error => {
-      arelt("投稿の表示ができません")
+      alert("投稿の表示ができません")
       console.log("投稿表示NG")
       console.log(error)
     })
@@ -55,11 +55,20 @@ class CommentList extends React.Component {
 
 // 投稿フォーム
 class CommentForm extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      // 投稿
+      name: '',
+      comment: ''
+    };
+    this.handleChange = this.handleChange.bind(this)
+  } 
   // 投稿処理 
-  post() {
+  post(e) {
     const url       = "http://localhost:5000/post";
-    const name      = document.getElementById("name").value
-    const comment   = document.getElementById("comment").value
+    const name      = this.state.name
+    const comment   = this.state.comment
     const data      = {user: name, comment: comment}
     // 入力チェック
     if (name    === "" || name    === null ||
@@ -68,23 +77,27 @@ class CommentForm extends React.Component {
       return false
     }
     // 投稿が完了したら投稿フォームを初期化する 
-    document.getElementById("name").value = ""
-    document.getElementById("comment").value = ""
+    this.setState({name: '', comment: ''})
     // 投稿内容を登録
     axios.post(url, data).then(res => {
     })
     .catch(error => {
-      arelt("コメントの投稿に失敗しました")
+      alert("コメントの投稿に失敗しました")
       console.log("新規投稿NG")
       console.log(error)
     });
   }
+
+  handleChange(e) {
+    this.setState({[e.target.name]: e.target.value})
+  }
+  
   render() {
     return (
       <div className="commentForm">
         <h2>投稿フォーム</h2>
-        <p><input type="text" id="name" name="name" placeholder="名前" /></p>       
-        <p><textarea id="comment" name="comment" cols="60" rows="15" maxLength="80" wrap="hard" placeholder="本文" /></p>
+        <p><input type="text" id="name" name="name" placeholder="名前" value={this.state.name} onChange={this.handleChange}/></p>       
+        <p><textarea id="comment" name="comment" cols="60" rows="15" maxLength="80" wrap="hard" placeholder="本文" value={this.state.comment} onChange={this.handleChange}/></p>
         <p><button className={css.btn} onClick={this.post.bind(this)}>投稿</button></p>
       </div>
     );
